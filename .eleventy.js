026153d862abe398668d7e222139e0840e5f0f86
anna.lopatinski <anna.lopatinski@myheritage.com>
2022-06-09 12:53:50 +0300

    let's try to serve img

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addWatchTarget("./src/css/");
-  return {
-    dir: {
-      input: "src",
-      output: "public",
-    },
-  };
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('assets')
+    eleventyConfig.addPassthroughCopy("./src/assets/css");
+    eleventyConfig.addWatchTarget("./src/assets/css/");
+    eleventyConfig.addPassthroughCopy("./src/assets/images");
+    return {
+        dir: {
+            passthroughFileCopy: true,
+            input: "src",
+            output: "public",
+        },
+    };
 };
